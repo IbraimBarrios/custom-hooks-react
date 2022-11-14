91c26a0ee6990edade91b2eb2484ec41bb47d849
Ibraim Barrios <barrios.party@hotmail.com>
2022-11-13 20:07:32 -0600

    feat: statefull

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import HeadingTitle from './components/HeadingTitle'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div>
       <HeadingTitle title={"Sistema de progreso"}/>
-      <div className="App">
+      <div className="App center">
         <div>
-
+          <Counter />
         </div>
       </div>
     </div>
